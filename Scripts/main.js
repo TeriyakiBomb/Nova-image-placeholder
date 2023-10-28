12f947752d5d979cb6451f546f619a20c2b0413a
Ash Joseph <kamikazedesigns@gmail.com>
2023-10-28 13:50:15 +0100

    First iteration

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,57 +1,73 @@
+function parseDimensions(input) {
+  const regexes = [
+    /^\d+\s\d+$/,
+    /^w\d+\sh\d+$/,
+    /^\d+,\d+$/,
+    /^\d+\/\d+$/,
+    /^\d+$/,
+  ];
 
-exports.activate = function() {
-    // Do work when the extension is activated
+  for (const regex of regexes) {
+    if (regex.test(input)) {
+      const [width, height] = input.match(/\d+/g);
+      return `${width}${height ? `/${height}` : ""}`;
+    }
+  }
+
+  return "Invalid input format";
 }
 
-exports.deactivate = function() {
-    // Clean up state before the extension is deactivated
+function parseDimensionsRandom(input) {
+  const regexes = [
+    /^\d+\s\d+$/,
+    /^w\d+\sh\d+$/,
+    /^\d+,\d+$/,
+    /^\d+\/\d+$/,
+    /^\d+$/,
+  ];
+
+  for (const regex of regexes) {
+    if (regex.test(input)) {
+      const [width, height] = input.match(/\d+/g);
+      const randomNum = Math.floor(Math.random() * 999) + 1;
+      return `${width}${height ? `/${height}` : ""}?random=${randomNum}`;
+    }
+  }
+
+  return "Invalid input format";
 }
 
-
-nova.commands.register("lorem-picsum.openURL", (workspace) => {
-    var options = {
-        "placeholder": "https://foobar.com",
-        "prompt": "Open"
-    };
-    nova.workspace.showInputPanel("Enter the URL to open:", options, function(result) {
-        if (result) {
-            nova.openURL(result, function(success) {
-                
-            });
-        }
-    });
+nova.commands.register("lorem-picsum.insertImagePlaceholder", (editor) => {
+  let generator = "https://picsum.photos/";
+  var options = {};
+  nova.workspace.showInputPalette(
+    "Enter width and then height",
+    options,
+    function (result) {
+      // Rename the parameter from 'editor' to 'inputEditor'
+      if (result) {
+        let dimensions = parseDimensions(result);
+        editor.insert(generator + dimensions);
+      }
+    }
+  );
 });
 
-
-nova.commands.register("lorem-picsum.runExternalTool", (workspace) => {
-    var options = {
-        "placeholder": "/path/to/tool",
-        "prompt": "Run"
-    };
-    nova.workspace.showInputPanel("Enter the path to the external tool:", options, function(result) {
+nova.commands.register(
+  "lorem-picsum.insertImagePlaceholderRandom",
+  (editor) => {
+    let generator = "https://picsum.photos/";
+    var options = {};
+    nova.workspace.showInputPalette(
+      "Enter width and then height",
+      options,
+      function (result) {
+        // Rename the parameter from 'editor' to 'inputEditor'
         if (result) {
-            var options = {
-                // "args": [],
-                // "env": {},
-                // "stdin": <any buffer or string>
-            };
-            
-            var process = new Process(result, options);
-            var lines = [];
-            
-            process.onStdout(function(data) {
-                if (data) {
-                    lines.push(data);
-                }
-            });
-            
-            process.onDidExit(function(status) {
-                var string = "External Tool Exited with Stdout:\n" + lines.join("");
-                nova.workspace.showInformativeMessage(string);
-            });
-            
-            process.start();
+          let dimensions = parseDimensionsRandom(result);
+          editor.insert(generator + dimensions);
         }
-    });
-});
-
+      }
+    );
+  }
+);
